c03e48352a45991fbd5f8b41b374f07459c56de2
Alessandra Aleluia <alessandra.aleluia@gmail.com>
2017-11-04 11:14:00 -0300

    User updated

--- src/main/Book.java
+++ src/main/Book.java
@@ -3,6 +3,19 @@ package main;
 public class Book {
 	
 	String name;
-	int price;
+	float price;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}	
 
 }
--- src/main/User.java
+++ src/main/User.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class User {
 	String name;
@@ -16,8 +17,65 @@ public class User {
 	
 	/* USER METHODS:
 	 * - addToCart (add a book to shoppingCart)
+	 * - showShoppingCart
 	 * - buyBook (pick up shoppingCart and choose payment method)
 	 * */
+	
+	public void addToCart(Book book, int quantity){
+		for(int i=0; i<quantity; i++)
+			this.shoppingCart.add(book);
+		
+	}
+	
+	public void showShoppingCart(){
+		float sum=0;
+		System.out.println("----------CARRINHO DE COMPRAS:----------");
+		System.out.println("Lista de Livros: \n");
+		for(int i=0; i<this.shoppingCart.size(); i++){
+			System.out.println(this.shoppingCart.get(i).getName());
+			sum += this.shoppingCart.get(i).getPrice();
+		}
+		System.out.printf("TOTAL A PAGAR: %f \n", sum);
+	}
+	
+	public boolean buyBook(){
+		float sum=0;
+		for(int i=0; i<this.shoppingCart.size(); i++){
+			sum += this.shoppingCart.get(i).getPrice();
+		}
+		System.out.printf("TOTAL A PAGAR: %f \n", sum);
+		if(paymentMethod()){
+			System.out.println("Pagamento realizado com sucesso!");
+			this.shoppingCart.clear();
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean paymentMethod(){
+		System.out.println("Qual o método de pagamento? \n" + "1 - PagSeguro \n" + "2 - Boleto \n" + "3 - Cartão de crédito");
+		Scanner in = new Scanner(System.in);
+		int choice = in.nextInt();
+		boolean result = false;
+		switch(choice){
+			case 1:
+				System.out.println("Coletando dados do PagSeguro");
+				result = true;
+				break;
+			case 2:
+				System.out.println("Gerando boleto");
+				result = true;
+				break;
+			case 3:
+				System.out.println("Coletando dados de cartão de crédito"); //can add more things here
+				result = true;
+				break;
+			default:
+				System.out.println("Opção inválida!");
+				break;
+		}
+		return result;
+	}
 	
 
 }
