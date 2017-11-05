498868663d8c52e17ff8d335f41d172a458e30ae
Juliana Cavalcanti <juliana.cavalcantic@gmail.com>
2017-11-05 02:47:52 -0200

    Bug Correction in JDBC.java
    JDBC.java updated, SearchEngine.java created and Connector/J.jar was added into the classpath

--- src/database/JDBC.java
+++ src/database/JDBC.java
@@ -6,11 +6,11 @@ public class JDBC {
 	
 	/*JDBC driver name and database URL*/
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-	static final String DB_URL = "jdbc:mysql://localhost/";
+	static final String DB_URL = "jdbc:mysql://localhost/library";
 
 	/*database credentials*/
-	static final String USER = "username";
-	static final String PASS = "password";
+	static final String USER = "root"; //username
+	static final String PASS = "root"; //password
 	
 	public static void main(String[] args){
 		Connection conn = null;
@@ -25,47 +25,55 @@ public class JDBC {
 			System.out.println("Creating database...");
 			stmt = conn.createStatement();
 			
-			String db = "CREATE DATABASE library";
-			stmt.executeUpdate(db);
+			//String db = "CREATE DATABASE library";
+			//stmt.executeUpdate(db);
 			
-			String table = "CREATE TABLE books ("
+			/*String table = "CREATE TABLE books ("
 					+ "id int NOT NULL,"
 					+ "title varchar(255),"
-					+ "author varchar(255)"
-					+ "price float,"
-					+ "promotionalPrice float);";
-			stmt.executeUpdate(table);
+					+ "author varchar(255),"
+					+ "price float);";
+			stmt.executeUpdate(table);*/
 			
-			String content = "INSERT INTO TABLE books (id, title, autor, price)"
-					+ "VALUES ('1', 'Cálculo Volume I', 'James Stewart', '84.90');"
-					
-					+  "INSERT INTO TABLE books (id, title, autor, price)"
-					+ "VALUES ('2', 'Cálculo Volume II', 'James Stewart', '89.90');"
-					
-					+ "INSERT INTO TABLE books (id, title, autor, price)"
-					+ "VALUES ('3', 'Fundamentos de Física Volume I', 'David Halliday, Robert Resnick', '109.90');"
-					
-					+ "INSERT INTO TABLE books (id, title, autor, price)"
-					+ "VALUES ('4', 'Conceitos de Linguagem de Programação', 'Robert Sebesta', '125.90');"
-					 
-					+"INSERT INTO TABLE books (id, title, autor, price)"
-					+ "VALUES ('5', 'Algoritmos - Teoria e Prática', 'Thomas Cormem', '282.90');"
-					
-					+  "INSERT INTO TABLE books (id, title, autor, price)"
-					+ "VALUES ('6', 'Introdução a Programação com a Linguagem C', 'Rodrigo de Barros Paes', '55.90');"
-					
-					+ "INSERT INTO TABLE books (id, title, autor, price)"
-					+ "VALUES ('7', 'Inteligência Artificial', 'Peter Norvig', '307.90');"
-					
-					+ "INSERT INTO TABLE books (id, title, autor, price)"
-					+ "VALUES ('8', 'O Guia do Mochileiro das Galáxias', 'Douglas Adams', '19.90');"
-					 
-					+"INSERT INTO TABLE books (id, title, autor, price)"
-					+ "VALUES ('9', 'Origem', 'Dan Brown', '49.90');"
-					
-					+"INSERT INTO TABLE books (id, title, autor, price)"
-					+ "VALUES ('10', 'O Mundo de Sofia', 'Jostein Gaarder', '39.90');" ;
-					stmt.executeUpdate(content);
+			String content = "INSERT INTO books (id, title, author, price)"
+					+ " VALUES ('1', 'Cálculo Volume I', 'James Stewart', '84.90');" ;
+			stmt.executeUpdate(content);
+			
+			content = "INSERT INTO books (id, title, author, price)"
+					+ " VALUES ('2', 'Cálculo Volume II', 'James Stewart', '89.90');";
+			stmt.executeUpdate(content);
+			
+			content = "INSERT INTO books (id, title, author, price)"
+					+ " VALUES ('3', 'Fundamentos de Física Volume I', 'David Halliday, Robert Resnick', '109.90');";
+			stmt.executeUpdate(content);
+			
+			content = "INSERT INTO books (id, title, author, price)"
+					+ " VALUES ('4', 'Conceitos de Linguagem de Programação', 'Robert Sebesta', '125.90');";
+			stmt.executeUpdate(content);
+			
+			content = "INSERT INTO books (id, title, author, price)"
+					+ " VALUES ('5', 'Algoritmos - Teoria e Prática', 'Thomas Cormem', '282.90');";
+			stmt.executeUpdate(content);
+			
+			content = "INSERT INTO books (id, title, author, price)"
+					+ " VALUES ('6', 'Introdução a Programação com a Linguagem C', 'Rodrigo de Barros Paes', '55.90');";
+			stmt.executeUpdate(content);
+			
+			content = "INSERT INTO books (id, title, author, price)"
+					+ " VALUES ('7', 'Inteligência Artificial', 'Peter Norvig', '307.90');";
+			stmt.executeUpdate(content);
+			
+			content = "INSERT INTO books (id, title, author, price)"
+					+ " VALUES ('8', 'O Guia do Mochileiro das Galáxias', 'Douglas Adams', '19.90');";
+			stmt.executeUpdate(content);
+			
+			content = "INSERT INTO books (id, title, author, price)"
+					+ " VALUES ('9', 'Origem', 'Dan Brown', '49.90');";
+			stmt.executeUpdate(content);
+			
+			content = "INSERT INTO books (id, title, author, price)"
+					+ " VALUES ('10', 'O Mundo de Sofia', 'Jostein Gaarder', '39.90');";
+			stmt.executeUpdate(content);
 			
 			System.out.println("Database created successfully!");
 		} catch (SQLException se) {
